978c4af0a68e38530304703de37246410b243cc0
djfusco <davidjfusco@gmail.com>
2022-04-19 13:06:47 -0400

    Cleaned up return values
    return values only guid

--- pages/api/getguidaws.js
+++ pages/api/getguidaws.js
@@ -6,41 +6,31 @@ export default async function (req, res) {
     const valueID = hash(id, {algorithm: 'md5'});
 
     if (req.method === 'GET') {
-    const params = {
-        TableName: 'GUID',
-        Key: {
-            haxuguid: id
+        const params = {
+            TableName: 'GUID',
+            Key: {
+                haxuguid: id
+            }
+        };
+        var result = await db.get(params).promise();
+        console.log(JSON.stringify(result));
+        return res.json(result.Item.guid);
+
+     } else if (req.method === 'PUT') {
+        const params = {
+            TableName: 'GUID',
+            Item: {
+                "haxuguid":  id,
+                "guid": valueID
+            }
         }
-      };
-    
-    
-    ///var result = await db.scan(params).promise();
-    var result = await db.get(params).promise();
-
-
-    console.log(JSON.stringify(result));
-
-  } else if (req.method === 'PUT') {
-    const params = {
-        TableName: 'GUID',
-        Item: {
-            "haxuguid":  id,
-            "guid": valueID
-          }
-    }
-
-    db.put(params, function(err, data) {
+        var result = db.put(params, function(err, data) {
             if (err) {
               console.error("Can't add data");
             } else {
               console.log("Succeeded adding an item for: ", id, " with guid: ", valueID);
+              return res.json(valueID);
             }
           });
-    
-
-
     };
-
-  res.statusCode = 201;
-  return res.json(JSON.stringify(result));
 }
