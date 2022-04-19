6985f2dca994ac5557191c75ba1f8556cd3df3b6
djfusco <davidjfusco@gmail.com>
2022-04-19 07:46:04 -0400

    get and put working
    update get and put

--- pages/api/getguidaws.js
+++ pages/api/getguidaws.js
@@ -2,29 +2,45 @@ import db from "./db";
 
 export default async function (req, res) {
     const id = req.body.uid;
+    var hash = require('object-hash');
+    const valueID = hash(id, {algorithm: 'md5'});
 
     if (req.method === 'GET') {
     const params = {
-        TableName: 'GUID'
+        TableName: 'GUID',
+        Key: {
+            haxuguid: id
+        }
       };
-/*
-    db.getItem(params, function (err, data) {
-      if (err) {
-        console.log('Error', err);
-      } else {
-        // send the json response from the callback
-        res.json(data.Item);
-      }
-    });
-*/
+    
+    
+    ///var result = await db.scan(params).promise();
+    var result = await db.get(params).promise();
 
-    var result = await db.scan(params).promise();
+
     console.log(JSON.stringify(result));
 
-
   } else if (req.method === 'PUT') {
-    // Allow a blog post to update its likes (via a button) or views (on rendering)
-  }
+    const params = {
+        TableName: 'GUID',
+        Item: {
+            "haxuguid":  id,
+            "guid": valueID
+          }
+    }
+
+    db.put(params, function(err, data) {
+            if (err) {
+              console.error("Can't add data");
+            } else {
+              console.log("Succeeded adding an item for: ", id, " with guid: ", valueID);
+            }
+          });
+    
+
+
+    };
+
   res.statusCode = 201;
   return res.json(JSON.stringify(result));
 }
